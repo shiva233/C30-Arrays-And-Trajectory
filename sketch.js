6afc1fcff1a9c2a30da5439d89f41c120a19c0b7
shiva233 <shivaprakash233@gmail.com>
2020-09-16 21:36:39 -0400

    arrays

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState = "onSling";
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +71,19 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState !== "Launched")
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "Launched"
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
+
     }
 }
